7134ba7f608edefe5a26f0d8aada6a6ebbe6a125
IsaiahHarris <isaiah.harris.hi@gmail.com>
2022-10-20 13:34:26 -1000

    add more colors

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,21 +13,19 @@ import {
   StatusBar,
 } from 'react-native';
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
+const colors = [
+  'black',
+  'white',
+  'yellow',
+  'orange',
+  'red',
+  'purple',
+  'magenta',
+  'green',
+  'teal',
+  'blue',
+  'black_and_white',
 ];
-const colors = ['black', 'white'];
 
 const orientations = ['landscape', 'portrait'];
 
